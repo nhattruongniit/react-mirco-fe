0e217318f820af7c989991d577daf897481ac3a6
truong.nn <truong.nn@amanotes.com>
2024-05-06 15:44:54 +0700

    doing setup

--- product-app/vite.config.ts
+++ product-app/vite.config.ts
@@ -1,8 +1,19 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "PRODUCT",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./product": "./src/pages/product.jsx",
+      },
+      shared: ["react"],
+    }),
+  ],
   server: {
     port: 8002,
   },
